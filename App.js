f5dc8ce478bfb5864178491b3f8ec85a56f9761f
yarwinliu <yarwinliu@gmail.com>
2020-06-27 21:40:51 -0700

    links were added

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Welcome to my website!
+        </h2>
+        <a
+          href="https://reactjs.org"
+        >
+        Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function testFunc(){
+  //console.log("test");
+  //document.getElementById('picture').innerHTML = ""
+  document.getElementById("link").href = "https://www.funimada.com/assets/images/cards/big/congrats-4.gif"
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className = "App-header">
+
+        <a id="picture" href="https://www.youtube.com/watch?v=5C0QU5xrAi4" target="blank">
+          <img src="https://raster-static.postmates.com/?url=com.postmates.img.prod.s3.amazonaws.com%2F29554206-b272-467e-88c6-6ad05efdadba%2Forig.jpg&quality=85&mode=auto&format=jpg&v=4&w=1200&h=720"
+            alt="croissant" height="100" border="2px blue"></img>
         </a>
-      </header>
+
+        
+        <h2>
+        Welcome to my website! 
+        </h2>
+
+        <div className = "App-link"> 
+          <a 
+            id="link"
+            href="https://reactjs.org"
+            target="blank"
+          > 
+          can you find the right link
+          </a>
+
+          <button
+            title = "are you sure?" 
+            onClick = {testFunc} 
+          > 
+            ?
+          </button>
+        </div>
+      </div>
     </div>
+    
   );
 }
-
 export default App;
