f32032329d84705964eb0ab63acd94bceab9b5da
yarwinliu <yarwinliu@gmail.com>
2020-07-05 12:46:35 -0700

    a loading bar was added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import React from 'react';
 import './App.css';
 
-function testFunc(){
-  //console.log("test");
-  //document.getElementById('picture').innerHTML = ""
+function linkFunc(){
+  //change the link of "can you find the right link"
   document.getElementById("link").href = "https://www.funimada.com/assets/images/cards/big/congrats-4.gif"
+}
 
+var i = 0;
+function move() {
+  if (i === 0) {
+    //move the progress bar 
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 1);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width +"%";
+      }
+    }
+  }
+}
+
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.getElementById("progBar").style.display = "flex  ";
+    document.getElementById("text").style.display = "none";
+    move(); 
+  }else{
+    setTimeout(() => {
+      console.log(document.readyState);
+      document.getElementById("progBar").style.display = "none";
+      document.getElementById("text").style.display = "flex";
+    }, 400);
+  }
 }
 
 function App() {
-  return (
+  return (      
     <div className="App">
-      <div className = "App-header">
 
-        <a id="picture" href="https://www.youtube.com/watch?v=5C0QU5xrAi4" target="blank">
+      <div class="progress" id="progBar">
+        <div class="bar" id="myBar"></div>
+      </div>
+
+      <div className = "App-header" id="text">
+
+        <a id="croissantpic" href="https://www.youtube.com/watch?v=5C0QU5xrAi4" target="blank">
           <img src="https://raster-static.postmates.com/?url=com.postmates.img.prod.s3.amazonaws.com%2F29554206-b272-467e-88c6-6ad05efdadba%2Forig.jpg&quality=85&mode=auto&format=jpg&v=4&w=1200&h=720"
             alt="croissant" height="100" border="2px blue"></img>
         </a>
-
         
         <h2>
         Welcome to my website! 
@@ -30,11 +66,11 @@ function App() {
             target="blank"
           > 
           can you find the right link
-          </a>
+          </a>  
 
           <button
             title = "are you sure?" 
-            onClick = {testFunc} 
+            onClick = {linkFunc} 
           > 
             ?
           </button>
